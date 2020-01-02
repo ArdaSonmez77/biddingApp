a6c7a5bd5eab5f2274c64d7c8bf081494db29ceb
ersinguven <35121627+ersinguven@users.noreply.github.com>
2020-01-02 03:35:39 +0300

    Update server_v0.1.py

--- server_v0.1.py
+++ server_v0.1.py
@@ -2,9 +2,9 @@ import socket
 import selectors
 import types
 import pickle
+import struct
 
-
-
+INFO_PATH = 'INFO.txt'
 def accept_wrapper(sock):
     conn, addr = sock.accept()
     print('Accepted connection from', addr)
@@ -34,18 +34,58 @@ def check_credentials(username, password):
                 return 1
     return 0
 
+def create_auction(a_name, a_desc, a_price, a_duration, sock):
+    path = 'Auctions\\'+a_name+'.txt'
+    img_path = 'Auctions\\'+a_name+'.png'
+    f = open(path, 'a')
+    f.write(a_name)
+    f.write('\n')
+    f.write(a_desc)
+    f.write('\n')
+    f.write(a_price)
+    f.write('\n')
+    f.write(a_duration)
+    f.write('\n')
+    f.close()
+
+    f_img = open(img_path,'wb')
+    size = sock.recv(4)
+    size = struct.unpack('!i', size)[0]
+    img_data = b''
+    while size > 0 :
+        if size >= 4096:
+            data = sock.recv(4096)
+        else:
+            data = sock.recv(size)
+
+        if not data:
+            break
+
+        size -= len(data)
+        img_data += data
+    f_img.write(img_data)
+    f_img.close()
+
+
+
+    return 1
+
 def service_connection(key, mask):
     sock = key.fileobj
     data = key.data
     if mask & selectors.EVENT_READ:
         recv_data = sock.recv(4096)
         inc_data = pickle.loads(recv_data)
+
         print(inc_data)
         if recv_data:
             if inc_data[0] == 0:
                 create_user(inc_data[1], inc_data[2])
             elif inc_data[0] == 1:
                 sock.send(bytes(str(check_credentials(inc_data[1], inc_data[2])),'utf-8'))
+            elif inc_data[0] == 2:
+                print(len(inc_data))
+                create_auction(inc_data[1], inc_data[2], inc_data[3], inc_data[4],sock)
 
 
 
